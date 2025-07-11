847a158319a0cd1f087712755788feea3d5a7d39
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2025-07-11 12:35:29 +0530

    Initial commit on main branch

---
+++ app/[[...sign-in]]/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import * as Clerk from "@clerk/elements/common";
+import * as SignIn from "@clerk/elements/sign-in";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const SignInPage = () => {
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    const signedInUser = user;
+    if (signedInUser) {
+      router.push("/dashboard");
+    }
+  }, [user, router]);
+
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <SignIn.Root>
+        <SignIn.Step name="start">
+          <h1>BG Sales Dashboard</h1>
+          <h2>Sign in to your account</h2>
+
+          <Clerk.GlobalError className="block text-sm text-red-400" />
+
+          <Clerk.Field name="identifier" className="space-y-2">
+            <Clerk.Label className="text-sm font-medium text-zinc-950">
+              Username
+            </Clerk.Label>
+            <Clerk.Input
+              type="text"
+              required
+              className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
+            />
+            <Clerk.FieldError className="block text-sm text-red-400" />
+          </Clerk.Field>
+
+          <Clerk.Field name="password" className="space-y-2">
+            <Clerk.Label className="text-sm font-medium text-zinc-950">
+              Password
+            </Clerk.Label>
+            <Clerk.Input
+              type="password"
+              required
+              className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
+            />
+            <Clerk.FieldError className="block text-sm text-red-400" />
+          </Clerk.Field>
+
+          <SignIn.Action
+            submit
+            className="w-full rounded-md bg-zinc-950 px-3.5 py-1.5 text-center text-sm font-medium text-white shadow outline-none ring-1 ring-inset ring-zinc-950 hover:bg-zinc-800 focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-zinc-950 active:text-white/70"
+          >
+            Sign In
+          </SignIn.Action>
+        </SignIn.Step>
+      </SignIn.Root>
+    </div>
+  );
+};
+
+export default SignInPage;
