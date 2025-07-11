35315044bfe96bf4b365a4b02ccc4189000f2949
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2025-07-11 13:11:52 +0530

    Sign In component done

--- app/[[...sign-in]]/page.tsx
+++ app/[[...sign-in]]/page.tsx
@@ -6,57 +6,81 @@ import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 const SignInPage = () => {
   const { user } = useUser();
   const router = useRouter();
 
   useEffect(() => {
-    const signedInUser = user;
-    if (signedInUser) {
-      router.push("/dashboard");
+    try {
+      const signedInUser = user;
+      if (signedInUser) {
+        router.push("/dashboard");
+      }
+    } catch (error) {
+      console.error(error);
     }
   }, [user, router]);
 
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center bg-gray-50">
       <SignIn.Root>
-        <SignIn.Step name="start">
-          <h1>BG Sales Dashboard</h1>
-          <h2>Sign in to your account</h2>
+        <Card className="w-full max-w-sm border-0 shadow-sm">
+          <SignIn.Step name="start">
+            <CardHeader>
+              <CardTitle className="text-center space-y-2 mb-3">
+                <h1 className="text-2xl font-medium text-gray-800">
+                  BG Sales Dashboard
+                </h1>
+                <h2 className="text-gray-500 font-normal">
+                  Sign in to your account
+                </h2>
+              </CardTitle>
+            </CardHeader>
 
-          <Clerk.GlobalError className="block text-sm text-red-400" />
+            <Clerk.GlobalError className="block text-sm text-red-400" />
 
-          <Clerk.Field name="identifier" className="space-y-2">
-            <Clerk.Label className="text-sm font-medium text-zinc-950">
-              Username
-            </Clerk.Label>
-            <Clerk.Input
-              type="text"
-              required
-              className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
-            />
-            <Clerk.FieldError className="block text-sm text-red-400" />
-          </Clerk.Field>
+            <CardContent className="space-y-4">
+              <Clerk.Field
+                name="identifier"
+                className="space-y-2 flex flex-col"
+              >
+                <Clerk.Label className="text-sm font-medium text-gray-700">
+                  Username
+                </Clerk.Label>
+                <Clerk.Input
+                  type="text"
+                  required
+                  className="border border-gray-400 rounded-md py-2 px-2"
+                />
+                <Clerk.FieldError className="block text-sm text-red-400" />
+              </Clerk.Field>
 
-          <Clerk.Field name="password" className="space-y-2">
-            <Clerk.Label className="text-sm font-medium text-zinc-950">
-              Password
-            </Clerk.Label>
-            <Clerk.Input
-              type="password"
-              required
-              className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
-            />
-            <Clerk.FieldError className="block text-sm text-red-400" />
-          </Clerk.Field>
+              <Clerk.Field
+                name="password"
+                className="space-y-2 flex flex-col pb-3"
+              >
+                <Clerk.Label className="text-sm font-medium text-gray-700">
+                  Password
+                </Clerk.Label>
+                <Clerk.Input
+                  type="password"
+                  required
+                  className="border border-gray-400 rounded-md py-2 px-2"
+                />
+                <Clerk.FieldError className="block text-sm text-red-400" />
+              </Clerk.Field>
 
-          <SignIn.Action
-            submit
-            className="w-full rounded-md bg-zinc-950 px-3.5 py-1.5 text-center text-sm font-medium text-white shadow outline-none ring-1 ring-inset ring-zinc-950 hover:bg-zinc-800 focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-zinc-950 active:text-white/70"
-          >
-            Sign In
-          </SignIn.Action>
-        </SignIn.Step>
+              <SignIn.Action
+                submit
+                className="w-full bg-gray-900 hover:bg-gray-600 text-white font-medium py-2.5 rounded-md cursor-pointer"
+              >
+                Sign In
+              </SignIn.Action>
+            </CardContent>
+          </SignIn.Step>
+        </Card>
       </SignIn.Root>
     </div>
   );
