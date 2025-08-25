3744f5c9de81797d88fbd30e6fc2ba7f2b42967e
Manmohan Khandelwal <kmanmohan925@gmail.com>
2025-08-25 12:02:37 +0530

    feat: restructure layout component to include ThemeProvider and SidebarProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import ApolloWrapper from "@/components/ApolloWrapper";
+import Navbar from "@/components/structures/Navbar";
+import { AppSidebar } from "@/components/structures/Sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { AuthProvider } from "@/hooks/AppContext";
+import { ThemeProvider } from "@/hooks/theme-provider";
 import type { Metadata } from "next";
 import { ToastContainer } from "react-toastify";
 import "./globals.css";
-import Navbar from "@/components/structures/Navbar";
 
 export const metadata: Metadata = {
   title: "Sales Dashboard",
@@ -12,22 +15,38 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <AuthProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className="antialiased bg-background-light dark:bg-background-dark">
-          <ToastContainer />
-          <ApolloWrapper>
-            <div className="absolute inset-0 overflow-hidden pointer-events-none">
-              <div className="absolute top-20 right-20 w-64 h-64 rounded-full blur-3xl opacity-10 bg-blue-400 dark:bg-blue-500" />
-              <div className="absolute bottom-20 left-20 w-48 h-48 rounded-full blur-3xl opacity-10 bg-purple-400 dark:bg-purple-500" />
-            </div>
-            <Navbar />
-            {children}
-          </ApolloWrapper>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <SidebarProvider>
+              <ApolloWrapper>
+                {/* Background decorations */}
+                <div className="absolute inset-0 overflow-hidden pointer-events-none">
+                  <div className="absolute top-20 right-20 w-64 h-64 rounded-full blur-3xl opacity-10 bg-blue-400 dark:bg-blue-500" />
+                  <div className="absolute bottom-20 left-20 w-48 h-48 rounded-full blur-3xl opacity-10 bg-purple-400 dark:bg-purple-500" />
+                </div>
+
+                {/* Navbar and Sidebar */}
+                <Navbar />
+                <AppSidebar />
+
+                <main className="relative flex flex-col items-center justify-start w-full pt-16">
+                  <ToastContainer />
+                  {children}
+                </main>
+              </ApolloWrapper>
+            </SidebarProvider>
+          </ThemeProvider>
         </body>
       </html>
     </AuthProvider>
